9fe65e4949aca167f49df83ee69ec09626a8b7b4
0x4164 <0x4164@github.com>
2018-03-01 10:48:25 +0700

    c1 init commit

---
+++ quotes.py
@@ -0,0 +1,78 @@
+import random
+
+quotes = ("""Our little systems have their day
+- Alfred, Lord Tennyson @ch 1""",
+"""From an article on a new operating system for the
+IBM PC: Real concurrency  in which one program actually continues to function while you call up and
+use another  is more amazing but of small use to the average person. How many programs do you have
+that take more than a few seconds to perform any
+task
+ New York Times, 25 April 1989 @ch 2""",
+"""One machine can do the work of fifty ordinary men.
+No machine can do the work of one extraordinary
+man.
+ Elbert Hubbard @ch 3""",
+"""As some day it may happen that a victim must be
+found, I've got a little list ....
+ W. S. Gilbert @ch 4""",
+"""What is called a sincere work is one that is endowed
+with enough strength to give reality to an illusion.
+ Max Jacob @ch 5""",
+"""When men willingly suspend fear, science flourishes.
+ Anonymous @ch 6""",
+"""The future belongs to him who knows how to wait.
+ Russian Proverb @ch 7""",
+"""The message of history is clear: the past lies before us.
+ Anonymous @ch 8""",
+"""The future belongs to him who knows how to wait.
+ Russian Proverb @ch 9""",
+"""Yea, from the table of my memory I'll wipe away all
+fond trivial records.
+ William Shakespeare @ch 10""",
+"""I'm always uneasy with messages.
+ Neil Tennant @ch 11""",
+"""The joy of music should never be interrupted by a
+commercial.
+ Leonard Bernstein @ch 12""",
+"""We haven't the time to take our time.
+ Eugene Ionesco @ch 13""",
+"""We have been left so much to our own devices  after
+a while, one welcomes the uncertainty of being left to
+other people's.
+ Tom Stoppard @ch 14""",
+"""It's hard to find a good driver these days, one with
+character and style.
+ Unknown @ch 15""",
+"""Modern hardware is stunningly difficult to deal with.
+ James Buchanan @ch 16""",
+"""The lure of the distant and the difficult is deceptive.
+The great opportunity is where you are.
+ John Burroughs @ch 17""",
+"""For my purpose holds ... To strive, to seek, to find,
+and not to yield.
+ Alfred, Lord Tennyson @ch 18""",
+"""Filing is concerned with the past; anything you
+actually need to see again has to do with the future.
+ Katharine Whitehorn @ch 19""",
+"""Networking makes the far-away the here-and-now.
+ Unknown @ch 20""",
+"""A rose by any other name ...
+ William Shakespeare @ch 21""",
+"""Only by avoiding the beginning of things can we
+escape their end.
+ Cyril Connolly  @ch 22""",
+"""A rose by any other name ...
+ William Shakespeare @ch 23""",
+"""The best thing about endings is knowing that just
+ahead lies the daunting task to start over
+ Jodi Picoult @ch 24""",
+"""I never make exceptions. An exception disproves the
+rule.
+ Sir Arthur Conan Doyle @ch 25""",
+"""No pleasure endures unseasoned by variety.
+ Publilius Syrus @ch 26""",
+"""A man has to learn that he cannot command things...
+ James Allen @ch 27"""
+)
+
+print random.choice(quotes)

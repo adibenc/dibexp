46ae9cbafc494339372533631100d82ec2d9077c
0x4164 <adib_okay@yahoo.co.id>
2018-12-23 03:00:39 +0700

    file generator fix1

--- pythons/dibmap/generate-dibmap.py
+++ pythons/dibmap/generate-dibmap.py
@@ -2,7 +2,8 @@
 #th
 #cat dib.map.list.txt | xargs (echo 2019 | tee ) --> salah
 
-#crypto
+import os
+
 msgs=['done']
 
 f1 = open('dib.map.list.txt','r')
@@ -11,18 +12,35 @@ list1=list1.split('\n')
 
 tahun=19
 ext='.txt'
-# while True:
-	# try:
-		# tahun=int(input('masukkan tahun'))
-		# break
-	# except:
-		# print('coba lg')
+i=0
+while True:
+	try:
+		tahun=int(input('masukkan tahun : '))
+		break
+	except:
+		print('coba lg')
+		i+=1
+		if i > 3: 
+			print('hmm')
+			exit(0)
+
 tahun+=2000
+s='.' #separator
+gdir=str(tahun)
+
+os.mkdir(gdir)
 
 for l in list1:
-	fname=str(tahun)+'.'+l+ext
-	print(fname," ready")
-	f2=open(fname,'w')
-	f2.write(fname)
+	for i in range(1,3):
+		fname='./'+gdir+'/'+str(tahun)+s+l+s+str(i)+ext
+		print(fname,"ready")
+		f2=open(fname,'w')
+		if i == 1:
+			fname+='\n0101'+str(tahun)
+		elif i == 2:
+			fname+='\n0107'+str(tahun)
+		
+		f2.write(fname)
+		f2.close()
 
 print(msgs[0])
